d79cf0f7f7797b5aae9c72a9d59dabac40e312c1
ramil <ramil1@gmail.com>
2023-08-21 15:56:23 +0400

    finished the third game

--- README.md
+++ README.md
@@ -4,3 +4,4 @@
 [![Maintainability](https://api.codeclimate.com/v1/badges/7619f82c24f0b0534ef9/maintainability)](https://codeclimate.com/github/Hex1er/frontend-project-44/maintainability)
 <a href="https://asciinema.org/a/cfW1CdGCwRxiDEkpNiMjes15B" target="_blank"><img src="https://asciinema.org/a/cfW1CdGCwRxiDEkpNiMjes15B.svg" /></a>
 <a href="https://asciinema.org/a/603406" target="_blank"><img src="https://asciinema.org/a/603406.svg" /></a>
+<a href="https://asciinema.org/a/603838" target="_blank"><img src="https://asciinema.org/a/603838.svg" /></a>
---
+++ src/games/gcd.js
@@ -0,0 +1,27 @@
+import { getRandom, heartOfGames } from '../index.js';
+
+const gcdGame = () => {
+  const gcdGameDescription = 'Find the greatest common divisor of given numbers.';
+  const gcdGameTask = () => {
+    const firstRandomNumber = getRandom(100);
+    const secondRandomNumber = getRandom(100);
+    const question = `${firstRandomNumber} ${secondRandomNumber}`;
+    const getGcd = (num1, num2) => {
+      let a = num1;
+      let b = num2;
+      while (a !== b) {
+        if (a > b) {
+          a -= b;
+        } else {
+          b -= a;
+        }
+      }
+      return a;
+    };
+    const res = getGcd(firstRandomNumber, secondRandomNumber).toString();
+    return [question, res];
+  };
+  heartOfGames(gcdGameDescription, gcdGameTask);
+};
+
+export default gcdGame;
