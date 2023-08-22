b1593154b4f504d9fd2c5c14e3abd8a72b33d1cf
ramil <ramil1@gmail.com>
2023-08-22 19:21:57 +0400

    code optimization

--- src/games/even.js
+++ src/games/even.js
@@ -2,13 +2,16 @@ import { getRandom, heartOfGames } from '../index.js';
 
 const evenGame = () => {
   const evenGameDescription = 'Answer "yes" if the number is even, otherwise answer "no".';
+
   const evenGameTask = () => {
-    const randomNumber = getRandom(100);
+    const question = getRandom(100);
     const isEven = (num) => num % 2 === 0;
-    const question = randomNumber;
-    const res = isEven(question) ? 'yes' : 'no';
-    return [question, res];
+
+    const result = isEven(question) ? 'yes' : 'no';
+
+    return [question, result];
   };
+
   heartOfGames(evenGameDescription, evenGameTask);
 };
 
